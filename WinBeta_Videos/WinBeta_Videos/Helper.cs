ee20093299979901d0d225f8439feac943a3afba
Andrey Zakharov <mediaexplorer74@hotmail.com>
2023-09-21 19:02:12 +0300

    .

--- WinBeta_Videos/WinBeta_Videos/Helper.cs
+++ WinBeta_Videos/WinBeta_Videos/Helper.cs
@@ -6,7 +6,7 @@ namespace WinBeta_Videos
     {
         internal static string GetApiKey()
         {
-            return "AIzaSyBo0ZFHKVnSs2RGxm8ckRVuBtSh1PQvL94"; // my Youtube API key
+            return "__"; // Youtube API key
         }
     }
 }
